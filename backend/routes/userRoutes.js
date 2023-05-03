0ccec7e6ac73ae87bd024150e7381f20fd185bc8
OdysseyACK <odyssey.ack@gmail.com>
2023-05-03 19:25:38 +0800

    Capstone-030523

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,242 @@
+import express from "express";
+import bcrypt from "bcryptjs";
+import expressAsyncHandler from "express-async-handler";
+import { isAuth, isAdmin, generateToken } from "../utils.js";
+import User from "../models/userModel.js";
+
+const userRouter = express.Router();
+
+// login
+userRouter.post(
+  "/login",
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      if (bcrypt.compareSync(req.body.password, user.password)) {
+        if (user.isActive === true) {
+          // check if the user is active
+          res.send({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isVendor: user.isVendor,
+            profilePic: user.profilePic,
+            isAdmin: user.isAdmin,
+            token: generateToken(user),
+          });
+          return;
+        } else {
+          res.status(401).send({
+            message:
+              "Account is disabled. Please contact admin for more information.",
+          });
+          return;
+        }
+      }
+    }
+    res.status(401).send({ message: "Invalid email or password" });
+  })
+);
+
+// update user profile
+userRouter.put(
+  "/account",
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+    if (user) {
+      user.name = req.body.name || user.name;
+      user.email = req.body.email || user.email;
+      user.profilePic = req.body.profilePic || user.profilePic;
+      if (req.body.password) {
+        user.password = bcrypt.hashSync(req.body.password, 8);
+      }
+
+      const updatedUser = await user.save();
+      res.send({
+        _id: updatedUser._id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+        profilePic: updatedUser.profilePic,
+        isAdmin: updatedUser.isAdmin,
+        token: generateToken(updatedUser),
+      });
+    } else {
+      res.status(404).send({ message: "User not found" });
+    }
+  })
+);
+
+// update vendor profile
+userRouter.put(
+  "/vaccount",
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+    if (user && user.isVendor) {
+      // only allow vendors to update their own accounts
+      user.name = req.body.name || user.name;
+      user.email = req.body.email || user.email;
+      user.profilePic = req.body.profilePic || user.profilePic;
+      user.vendorDesc = req.body.vendorDesc || user.vendorDesc;
+      user.vendorType = req.body.vendorType || user.vendorType;
+      user.vendorPrice = req.body.vendorPrice || user.vendorPrice;
+      if (req.body.password) {
+        user.password = bcrypt.hashSync(req.body.password, 8);
+      }
+
+      const updatedUser = await user.save();
+      res.send({
+        _id: updatedUser._id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+        profilePic: updatedUser.profilePic,
+        isAdmin: updatedUser.isAdmin,
+        isVendor: updatedUser.isVendor, // return the isVendor field in the response
+        vendorDesc: updatedUser.vendorDesc,
+        vendorType: updatedUser.vendorType,
+        vendorPrice: updatedUser.vendorPrice,
+        token: generateToken(updatedUser),
+      });
+    } else {
+      res.status(404).send({ message: "User not found" });
+    }
+  })
+);
+
+// register
+userRouter.post(
+  "/register",
+  expressAsyncHandler(async (req, res) => {
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      isVendor: req.body.isVendor,
+      profilePic: req.body.profilePic,
+      password: bcrypt.hashSync(req.body.password),
+    });
+    const user = await newUser.save();
+    res.send({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      profilePic: user.profilePic,
+      isVendor: user.isVendor,
+      isAdmin: user.isAdmin,
+
+      token: generateToken(user),
+    });
+  })
+);
+
+// fetch all users from db
+userRouter.get(
+  "/dashboard",
+  expressAsyncHandler(async (req, res) => {
+    try {
+      const fetchUsers = await User.find();
+      res.json(fetchUsers);
+    } catch (error) {
+      throw new Error(error);
+    }
+  })
+);
+
+userRouter.get(
+  "/",
+  expressAsyncHandler(async (req, res) => {
+    try {
+      const fetchUsers = await User.find();
+      res.json(fetchUsers);
+    } catch (error) {
+      throw new Error(error);
+    }
+  })
+);
+
+userRouter.put(
+  "/dashboard",
+  isAdmin,
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      user.isAdmin = true;
+      const updatedUser = await user.save();
+      res.send({
+        user: updatedUser,
+        token: generatedToken(updatedUser),
+      });
+    } else {
+      res.status(404).send({ message: "User Not Found" });
+    }
+  })
+);
+
+userRouter.get(
+  "/:id",
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      res.send(user);
+    } else {
+      res.status(404).send({ message: "User not found" });
+    }
+  })
+);
+
+// dashboard edit user
+userRouter.put(
+  "/:id",
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      user.isAdmin = Boolean(req.body.isAdmin);
+      user.isVendor = Boolean(req.body.isVendor);
+      user.isActive = Boolean(req.body.isActive);
+      const updatedUser = await user.save();
+      res.send({ message: "user Updated", user: updatedUser });
+    } else {
+      res.status(404).send({ message: "User Not Found" });
+    }
+  })
+);
+
+// delete user
+userRouter.delete(
+  "/:id",
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    try {
+      const user = await User.findById(req.params.id);
+      if (user) {
+        await user.deleteOne();
+        res.send({ message: "User Deleted" });
+      } else {
+        res.status(404).send({ message: "User Not Found" });
+      }
+    } catch (error) {
+      res.status(500).send({ message: "Error deleting user" });
+    }
+  })
+);
+
+// fetch only vendors from db
+userRouter.get(
+  "/vcalculator",
+  expressAsyncHandler(async (req, res) => {
+    try {
+      const fetchVendors = await User.find({ isVendor: true });
+      res.json(fetchVendors);
+    } catch (error) {
+      throw new Error(error);
+    }
+  })
+);
+
+export default userRouter;
---
+++ frontend/src/pages/Dashboard.js
@@ -0,0 +1,285 @@
+import { useState, useContext, useEffect, useReducer } from "react";
+import { Table } from "react-bootstrap";
+import axios from "axios";
+import {
+  MDBBtn,
+  MDBDropdown,
+  MDBDropdownMenu,
+  MDBDropdownToggle,
+  MDBDropdownItem,
+  MDBIcon,
+} from "mdb-react-ui-kit";
+import { toast } from "react-toastify";
+import { getError } from "../utils";
+import { useNavigate, useParams } from "react-router-dom";
+import { EventStore } from "../EventStore";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, loading: false };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    case "UPDATE_REQUEST":
+      return { ...state, loadingUpdate: true };
+    case "UPDATE_SUCCESS":
+      return { ...state, loadingUpdate: false };
+    case "UPDATE_FAIL":
+      return { ...state, loadingUpdate: false };
+    default:
+      return state;
+  }
+};
+
+function Dashboard() {
+  const [users, setUsers] = useState([]);
+  const [{ loading, error, loadingUpdate }, dispatch] = useReducer(reducer, {
+    loading: true,
+    error: "",
+  });
+
+  useEffect(() => {
+    axios
+      .get("/api/users/dashboard")
+      .then((response) => {
+        setUsers(response.data);
+        dispatch({ type: "FETCH_SUCCESS" });
+      })
+      .catch((error) => {
+        console.log(error);
+        dispatch({ type: "FETCH_FAIL", payload: getError(error) });
+      });
+  }, []);
+
+  const params = useParams();
+  const { id: userId } = params;
+  const navigate = useNavigate();
+
+  const { state } = useContext(EventStore);
+  const { userInfo } = state;
+
+  const makeAdmin = async (e, user) => {
+    e.preventDefault();
+    try {
+      if (!user._id) {
+        throw new Error("User ID is missing");
+      }
+      const updatedUser = { ...user, isAdmin: !user.isAdmin };
+      dispatch({ type: "UPDATE_REQUEST" });
+      await axios.put(`/api/users/${user._id}`, updatedUser, {
+        headers: { Authorization: `Bearer ${userInfo.token}` },
+      });
+      dispatch({
+        type: "UPDATE_SUCCESS",
+      });
+      toast.success(
+        `User ${
+          updatedUser.isAdmin ? "made admin" : "removed admin"
+        } successfully`
+      );
+      setUsers((prevUsers) =>
+        prevUsers.map((prevUser) =>
+          prevUser._id === updatedUser._id ? updatedUser : prevUser
+        )
+      );
+    } catch (error) {
+      toast.error(getError(error));
+      dispatch({ type: "UPDATE_FAIL" });
+    }
+  };
+
+  const makeVendor = async (e, user) => {
+    e.preventDefault();
+    try {
+      if (!user._id) {
+        throw new Error("User ID is missing");
+      }
+      const updatedUser = { ...user, isVendor: !user.isVendor };
+      dispatch({ type: "UPDATE_REQUEST" });
+      await axios.put(`/api/users/${user._id}`, updatedUser, {
+        headers: { Authorization: `Bearer ${userInfo.token}` },
+      });
+      dispatch({
+        type: "UPDATE_SUCCESS",
+      });
+      toast.success(
+        `User ${
+          updatedUser.isVendor ? "made vendor" : "removed vendor"
+        } successfully`
+      );
+      setUsers((prevUsers) =>
+        prevUsers.map((prevUser) =>
+          prevUser._id === updatedUser._id ? updatedUser : prevUser
+        )
+      );
+    } catch (error) {
+      toast.error(getError(error));
+      dispatch({ type: "UPDATE_FAIL" });
+    }
+  };
+
+  const notActive = async (e, user) => {
+    e.preventDefault();
+    try {
+      if (!user._id) {
+        throw new Error("User ID is missing");
+      }
+      const updatedUser = { ...user, isActive: !user.isActive };
+      dispatch({ type: "UPDATE_REQUEST" });
+      await axios.put(`/api/users/${user._id}`, updatedUser, {
+        headers: { Authorization: `Bearer ${userInfo.token}` },
+      });
+      dispatch({
+        type: "UPDATE_SUCCESS",
+      });
+      toast.success(
+        `User ${updatedUser.isActive ? "made active" : "disabled"} successfully`
+      );
+      setUsers((prevUsers) =>
+        prevUsers.map((prevUser) =>
+          prevUser._id === updatedUser._id ? updatedUser : prevUser
+        )
+      );
+    } catch (error) {
+      toast.error(getError(error));
+      dispatch({ type: "UPDATE_FAIL" });
+    }
+  };
+
+  const deleteUser = async (e, user) => {
+    e.preventDefault();
+    try {
+      if (!user._id) {
+        throw new Error("User ID is missing");
+      }
+      const confirmDelete = window.confirm(
+        `Are you sure you want to delete ${user.name}?`
+      );
+      if (confirmDelete) {
+        dispatch({ type: "UPDATE_REQUEST" });
+        await axios.delete(`/api/users/${user._id}`, {
+          headers: { Authorization: `Bearer ${userInfo.token}` },
+        });
+        dispatch({
+          type: "UPDATE_SUCCESS",
+        });
+        toast.success(`User deleted successfully`);
+        setUsers((prevUsers) =>
+          prevUsers.filter((prevUser) => prevUser._id !== user._id)
+        );
+      }
+    } catch (error) {
+      toast.error(getError(error));
+      dispatch({ type: "UPDATE_FAIL" });
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1 className="text-center" style={{ marginTop: "10%" }}>
+        Manage Users
+      </h1>
+
+      {loading && <div>Loading...</div>}
+      {error && <div>Error: {error}</div>}
+
+      {!loading && !error && (
+        <Table striped bordered hover className="mt-3 text-center">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>User Type</th>
+              <th>Admin Rights</th>
+              <th>Status</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={user._id}>
+                <td>{index + 1}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.isVendor ? "Vendor" : "User"}</td>
+                <td>{user.isAdmin ? "Admin" : "Not Admin"}</td>
+                <td>{user.isActive ? "Active" : "Disabled"}</td>
+                <td
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                  }}
+                >
+                  <MDBDropdown style={{ marginRight: "10px" }}>
+                    {user.email !== "admin@example.com" && (
+                      <MDBDropdownToggle color="info">Edit</MDBDropdownToggle>
+                    )}
+                    <MDBDropdownMenu>
+                      <MDBDropdownItem
+                        link
+                        onClick={async (e) => {
+                          await makeAdmin(e, user);
+                          // Update users state after making admin
+                          const updatedUsers = users.map((u) =>
+                            u._id === user._id
+                              ? { ...user, isAdmin: !user.isAdmin }
+                              : u
+                          );
+                          setUsers(updatedUsers);
+                        }}
+                      >
+                        {user.isAdmin ? "Remove Admin" : "Make Admin"}
+                      </MDBDropdownItem>
+                      <MDBDropdownItem
+                        link
+                        onClick={async (e) => {
+                          await makeVendor(e, user);
+                          // Update users state after making vendor
+                          const updatedUsers = users.map((u) =>
+                            u._id === user._id
+                              ? { ...user, isVendor: !user.isVendor }
+                              : u
+                          );
+                          setUsers(updatedUsers);
+                        }}
+                      >
+                        {user.isVendor ? "Remove Vendor" : "Make Vendor"}
+                      </MDBDropdownItem>
+                    </MDBDropdownMenu>
+                  </MDBDropdown>
+                  {user.email !== "admin@example.com" && (
+                    <MDBBtn
+                      style={{ marginRight: "10px" }}
+                      color="warning"
+                      onClick={async (e) => {
+                        await notActive(e, user);
+                        const updatedUsers = users.map((u) =>
+                          u._id === user._id
+                            ? { ...user, isActive: !user.isActive }
+                            : u
+                        );
+                        setUsers(updatedUsers);
+                      }}
+                    >
+                      {user.isActive ? "Disable" : "Reinstate"}
+                    </MDBBtn>
+                  )}
+                  {user.email !== "admin@example.com" && (
+                    <MDBBtn color="danger" onClick={(e) => deleteUser(e, user)}>
+                      <MDBIcon fas icon="trash" size="lg" />
+                    </MDBBtn>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
+    </div>
+  );
+}
+
+export default Dashboard;
