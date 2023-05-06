6bbfd90bb61570ad2ec9b9cea170f74bae16df14
OdysseyACK <odyssey.ack@gmail.com>
2023-05-06 23:17:50 +0800

    11.17pm

--- frontend/src/pages/Vcalculator.js
+++ frontend/src/pages/Vcalculator.js
@@ -208,10 +208,12 @@ function Vcalculator() {
 
   return (
     <div className="container vcalculator">
-      <h2>Select the ideal partners that suit your event.</h2>
-      <p>
-        Our partners will be in contact with you within the next working day!
-      </p>
+      <div className="vcalculator-header">
+        <h2>Select the ideal partners that suit your event.</h2>
+        <p>
+          Our partners will be in contact with you within the next working day!
+        </p>
+      </div>
       {loading && <div>Loading...</div>}
       {error && <div>Error: {error}</div>}
       <div className="v-content">
